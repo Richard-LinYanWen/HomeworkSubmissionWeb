44dd160bad35f7140c1136a42d5c3e1516d7e53b
Richard 林彥汶 <ri022005@gmail.com>
2026-03-29 18:57:25 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const submissionSchema = new mongoose.Schema({
+    studentName: { type: String, required: true }, // The "Who"
+    homeworkId: { type: String, required: true },  // The "Which one"
+    fileName: String,                             // The "What"
+    submittedAt: { type: Date, default: Date.now }
+});
+
+const Submission = mongoose.model('Submission', submissionSchema);
